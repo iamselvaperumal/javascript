839439099fa13cd7dd6ba9ebc9d8389ff3cb4f2e
Apple <apple@Apples-MacBook-Pro.local>
2025-01-13 17:05:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+// Variables
+let variableName = "welcome to variable";
+console.log(variableName);
+
+var Name = "Jhon";
+console.log(Name);
+
+const Age = "18";
+console.log(Age);
+
+// Data Types
+// String
+const SingleQurdes = "Hello";
+const doubleQurdes = "Hello";
+const backticks = `Hello`;
+
+console.log(SingleQurdes);
+console.log(doubleQurdes);
+console.log(backticks);
+
+const name = "jason";
+const concordenate = ` Hello, ${name}!!`;
+
+console.log(concordenate);
+
+const add = `${2 + 2}`;
+console.log(add);
+
+// to finding the datatype
+console.log(typeof add);
+
+// Numbers
+const wholenumber = 5;
+const decimalnumber = 0.5;
+
+console.log(wholenumber);
+console.log(decimalnumber);
+
+// logical operater
+const firstNum = 10;
+const secoundNum = 5;
+
+// here only change the operater
+let result = firstNum + secoundNum;
+console.log(result);
+
+// Booleans
+let boolen = firstNum < secoundNum;
+console.log(boolen);
+
+// conditional statement
+if (boolen) {
+  console.log("hayy Im Very cool");
+} else {
+  console.log("sorry Im Not Cool");
+}
+
+// Null
+let age = null;
+console.log(age);
+
+// Undefined
+let x;
+console.log(x);
