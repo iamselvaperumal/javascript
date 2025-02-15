755fa9b13aac4878d1b290638a2f62a20f637f6f
“Selvaperumal <dev.iamselva@gmail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global merge.conflictstyle diff3git config --global core.editor code>
2025-02-15 12:53:04 +0530

    next challenge

--- fundamentel2.js
+++ fundamentel2.js
@@ -54,3 +54,21 @@ const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(total);
+
+// code challenge2
+let red = "red",
+  blue = "blue",
+  pink = "pink";
+const balls = (colors) => {
+  if (colors == red) {
+    console.log("i don't like red color");
+  } else if (colors == blue) {
+    console.log("i really like the blue color");
+  } else if (colors == pink) {
+    console.log("i too like pink color");
+  } else {
+    console.log("i don't like any color");
+  }
+};
+const colors = ["red", "blue", "pink"];
+console.log(balls(colors[2]));
