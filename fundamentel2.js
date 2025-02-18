bbb2a7f114595b93c61883f806060bd0d45b6dc5
“Selvaperumal <dev.iamselva@gmail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global merge.conflictstyle diff3git config --global core.editor code>
2025-02-18 23:27:52 +0530

    next update

--- fundamentel2.js
+++ fundamentel2.js
@@ -61,16 +61,165 @@ let red = "red",
   pink = "pink";
 const balls = (colors) => {
   if (colors == red) {
-    console.log("i don't like red color");
+    return "red";
   } else if (colors == blue) {
-    console.log("i really like the blue color");
+    return console.log("i really like the blue color");
   } else if (colors == pink) {
-    console.log("i too like pink color");
+    return console.log("i too like pink color");
   } else {
-    return "i don't like grey";
+    return console.log("i don't like grey");
   }
 };
 const colors = ["red", "blue", "pink"];
 colors.push("grey");
 console.log(colors);
-console.log(balls(colors[3]));
+console.log(balls(colors[0]));
+
+// const student = {
+//   name: "Jason",
+//   age: 25,
+//   marks: {
+//     phy: 98,
+//     chem: 99,
+//     bio: 100,
+//   },
+//   total: function total() {
+//     return this.marks.bio + this.marks.chem + this.marks.phy;
+//   },
+// };
+
+// console.log(student.name);
+// console.log(student.age);
+// console.log(student.total());
+
+// const marks = ["hundred", 100, 200];
+// console.log(marks);
+
+const student = {
+  name: "selva",
+  age: 19,
+  dept: "cse",
+  marks: {
+    m1: 80,
+    m2: 90,
+    m3: 95,
+  },
+  total: function total() {
+    const sum = this.marks.m1 + this.marks.m2 + this.marks.m3;
+    return sum;
+  },
+};
+console.log(student.total());
+
+// code challenge 3
+const markinfo = {
+  name: "Mark Miller",
+  mass: 78,
+  hight: 1.69,
+  calcBmi: function () {
+    this.bmi = (this.mass / this.hight) * this.hight;
+    return this.bmi;
+  },
+};
+const johninfo = {
+  name: "John Smith",
+  mass: 92,
+  hight: 1.95,
+  calcBmi: function () {
+    this.bmi = (this.mass / this.hight) * this.hight;
+    return this.bmi;
+  },
+};
+console.log(markinfo.calcBmi());
+console.log(johninfo.calcBmi());
+
+const compare =
+  markinfo.bmi > johninfo.bmi
+    ? `Mark has higher BMI ${markinfo.calcBmi()}`
+    : `John has higher BMI ${johninfo.calcBmi()}`;
+
+console.log(compare);
+
+const students = [
+  {
+    name: "Jason",
+    age: 25,
+    marks: {
+      phy: 98,
+      chem: 99,
+      bio: 100,
+    },
+    total: function total() {
+      return this.marks.bio + this.marks.chem + this.marks.phy;
+    },
+  },
+  {
+    name: "selva",
+    age: 15,
+    marks: {
+      phy: 96,
+      chem: 97,
+      bio: 92,
+    },
+    total: function total() {
+      return this.marks.bio + this.marks.chem + this.marks.phy;
+    },
+  },
+  {
+    name: "dinesh",
+    age: 17,
+    marks: {
+      phy: 18,
+      chem: 29,
+      bio: 30,
+    },
+    total: function total() {
+      return this.marks.bio + this.marks.chem + this.marks.phy;
+    },
+  },
+];
+students.map((student) => {
+  console.log(student.name);
+  console.log(student.age);
+  console.log(student.total());
+});
+
+// code challenge
+
+const pays = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const gifts = [];
+const totals = [];
+
+function tipCalc(pay) {
+  return pay >= 50 && pay <= 300 ? pay * 0.15 : pay * 0.2;
+}
+for (let i = 0; i <= pays.length; i++) {
+  gifts.push(tipCalc(pays[i]));
+  totals.push(gifts[i] + pays[i]);
+}
+console.log(gifts);
+console.log(totals);
+
+function average(bill) {
+  let average = 0;
+  for (let i = 0; i <= bill.length; i++) {
+    average += bill[i];
+  }
+  return average / bill.length;
+}
+console.log(average(totals));
+
+const temperatures = [3, -2, -6, "error", 9, 13, 17, 15, 14, 9, 5];
+const calcTemp = function (temperature) {
+  let min = temperature[0];
+  let max = temperature[0];
+  for (let i = 1; i < temperature.length; i++) {
+    const currentTemp = temperature[i];
+    if (typeof currentTemp !== "number") continue;
+    if (currentTemp > max) max = currentTemp;
+    if (currentTemp < min) min = currentTemp;
+  }
+  console.log(min, max);
+  return max - min;
+};
+console.log(calcTemp(temperatures));
