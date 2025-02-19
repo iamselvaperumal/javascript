f39a71c2419ac6aa8b1edc02941eca86fe05f8bf
“Selvaperumal <dev.iamselva@gmail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global merge.conflictstyle diff3git config --global core.editor code>
2025-02-19 11:23:30 +0530

    splited

---
+++ developerskill.js
@@ -0,0 +1,59 @@
+// developer Skills
+
+const pays = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const gifts = [];
+const totals = [];
+
+function tipCalc(pay) {
+  return pay >= 50 && pay <= 300 ? pay * 0.15 : pay * 0.2;
+}
+for (let i = 0; i <= pays.length; i++) {
+  gifts.push(tipCalc(pays[i]));
+  totals.push(gifts[i] + pays[i]);
+}
+console.log(gifts);
+console.log(totals);
+
+function average(bill) {
+  let average = 0;
+  for (let i = 0; i <= bill.length; i++) {
+    average += bill[i];
+  }
+  return average / bill.length;
+}
+console.log(average(totals));
+
+const temperatures = [3, -2, -6, "error", 9, 13, 17, 15, 14, 9, 5];
+const calcTemp = function (temperature) {
+  let min = temperature[0];
+  let max = temperature[0];
+  for (let i = 1; i < temperature.length; i++) {
+    const currentTemp = temperature[i];
+    if (typeof currentTemp !== "number") continue;
+    if (currentTemp > max) max = currentTemp;
+    if (currentTemp < min) min = currentTemp;
+  }
+  console.log(min, max);
+  return max - min;
+};
+// temperatures.push * [82, -40, 24];
+console.log(calcTemp(temperatures));
+
+const temp1 = [3, -2, -6, "error", 9, 13, 17, 15, 14, 9, 5];
+const temp2 = [8, 12, 21];
+calcTempNew = function (temp1, temp2) {
+  const temp = temp1.concat(temp2);
+  let min = temp[0];
+  let max = temp[0];
+  for (let i = 1; i < temp.length; i++) {
+    const currentTemp = temp[i];
+    if (typeof currentTemp !== "number") continue;
+    if (currentTemp > max) max = currentTemp;
+    if (currentTemp < min) min = currentTemp;
+  }
+  console.log(temp);
+  console.log(min, max);
+  return max - min;
+};
+
+console.log(calcTempNew(temp1, temp2));
